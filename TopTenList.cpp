d364c002a77b4d55a4e77974604ba593f9f94b4c
Jonathan Cheon <jonathan.cheon@gmail.com>
2020-04-02 15:49:49 -0700

    Structural Design Patterns and Interfaces

--- TopTenList.cpp
+++ TopTenList.cpp
@@ -1,21 +1,41 @@
 //Manages a list of top ten hyperlinks
+#include <iostream>
 #include <string>
 #include "TopTenList.h"
 #include "Hyperlink.h"
 
 using std::string;
+using std::cout;
 
 TopTenList::TopTenList()
 {
+    _list.resize(10);
+}
 
+void TopTenList::display_forward()
+{
+    cout << "Programming Languages Top Ten List\n";
+    for(int i = 1; i < _list.size(); i++)
+    {
+        cout << i << ") " << get(i).text << " ("<<get(i).url<<")\n";
+    }
+}
+
+void TopTenList::display_backward()
+{
+    cout << "Programming Languages Top Ten List Backwards\n";
+    for(int i = _list.size(); i>=1; i--)
+    {
+        cout << i << ") "<<get(i).text<<" ("<<get(i).url<<")\n";
+    }
 }
 
 void TopTenList::set_at(int index, Hyperlink link)
 {
-
+    _list.at(index-1) = link;
 }
 
 Hyperlink TopTenList::get(int index)
 {
-
+    return _list.at(index-1);
 }
